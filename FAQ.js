a6b4827b8ccc874ac3fd3ac5ed0fea306477403e
Passion-cyber <chineduibelachi@gmail.com>
2023-01-15 14:30:40 +0100

    Finalised

--- FAQ.js
+++ FAQ.js
@@ -1,25 +1,16 @@
-const header = document.getElementsByClassName('header');
+const headers = document.getElementsByClassName('header');
 const contents = document.getElementsByClassName('content');
 const plusIcon = document.getElementsByClassName('plus-icon');
 
 
 // FAQ SECTION
-for (let i = 0; i < header.length; i++) {
-  header[i].addEventListener('click', () => {
-    contents[i].style.display = contents[i].style.display == "block" ? "none" : "block";
-    // plusIcon[i].innerHTML = contents[i].style.display == "block" ? "-" : "+";
-  });
-}
-
 for (let i = 0; i < headers.length; i++) {
   headers[i].addEventListener("click", () => {
       for (let j = 0; j < contents.length; j++) {
           if (i == j) {
               contents[j].style.display = contents[j].style.display == "block" ? "none" : "block";
-              // plusIcon[j].innerHTML = contents[j].style.display == "block" ? "-" : "+";
           } else {
               contents[j].style.display = "none";
-              plusIcon[j].innerHTML = "+";
           }
       }
   });
